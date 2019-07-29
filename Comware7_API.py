09213500495ab096996332b230250aeffbeffce0
Paul-kim <paul.kim@hpe.com>
2019-07-29 21:33:19 +0900

    init commit

---
+++ Comware7_API.py
@@ -0,0 +1,125 @@
+# pip install requests
+# -*- coding: utf-8 -*-
+import requests, json, base64, sys
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning 
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+API_BASE = "https://{COMWARE7_IP}"
+API_AUTH = "YOUR_LOGIN_ID:YOUR_LOGIN_PW"
+
+if len(sys.argv) < 3 :
+    print("python3 Comware7_API.py {add|del} acct_name")
+    exit()
+
+mode = sys.argv[1]
+acct_name = sys.argv[2]
+
+#1. Get Token
+#POST /api/v1/tokens
+print("-> Get tokens")
+auth = base64.b64encode(API_AUTH.encode("ascii")).decode("ascii")
+headers = {"Authorization": "Basic %s" % auth, 'Content-Type':'application/json'}
+
+try:
+    API_URL = "/api/v1/tokens"
+    r = requests.post(API_BASE+API_URL, headers=headers, verify=False)
+    r.raise_for_status()
+except Exception as e:
+    print(e)
+    exit(1)
+json_response = json.loads(r.text)
+tokenid = json_response['token-id']
+print(" - " + tokenid)
+print("//=======")
+
+
+#2.Get information for all device management users.
+#GET /api/v1/UserAccounts/Management/Accounts
+print("-> Get users.")
+headers = {"X-Auth-Token": tokenid, 'Content-Type':'application/json'}
+
+try:
+    API_URL = "/api/v1/UserAccounts/Management/Accounts"
+    r = requests.get(API_BASE+API_URL, headers=headers, verify=False)
+    r.raise_for_status()
+except Exception as e:
+    print(e)
+    exit(1)
+json_response = json.loads(r.text)
+acct_list = json_response['Accounts']
+for i in range(len(acct_list)): 
+    print(" - Name : " + str(acct_list[i]['Name']))
+print("//=======")
+
+
+if mode == "add" :
+    #3.Add a device management user.
+    #POST /api/v1/UserAccounts/Management/Accounts
+    #{"Name": "qwer"}
+    print("-> Add user.")
+    headers = {"X-Auth-Token":tokenid,'Content-Type':'application/json'}
+    payload = {"Name":acct_name,"HTTPS":"true","SSH":"true"}
+    
+    try:
+        API_URL = "/api/v1/UserAccounts/Management/Accounts"
+        r = requests.post(API_BASE+API_URL, headers=headers, json=payload, verify=False)
+        r.raise_for_status()
+    except Exception as e:
+        print(e)
+        exit(1)
+        print("//=======")
+
+    #4. Add a password for a user.
+    #POST /api/v1/UserAccounts/ChangePasswords
+    #{"UserName": "admin","Password": "asd "}
+    print("-> Setup password")
+    headers = {"X-Auth-Token": tokenid, 'Content-Type':'application/json'}
+    payload = {"UserName":acct_name,"Password":"P@ssw0rd"}
+    
+    try:
+        API_URL = "/api/v1/UserAccounts/ChangePasswords"
+        r = requests.post(API_BASE+API_URL, headers=headers, json=payload, verify=False)
+        r.raise_for_status()
+    except Exception as e:
+        print(e)
+        exit(1)
+        print("//=======")
+
+elif mode == "del" :
+    #OPT. Delete User
+    #DELETE /api/v1/UserAccounts/Management/Accounts?index=Name=test;
+    print("-> delete user")
+    headers = {"X-Auth-Token": tokenid, 'Content-Type':'application/json'}
+    
+    try:
+        API_URL = "/api/v1/UserAccounts/Management/Accounts?index=Name="+acct_name
+        r = requests.delete(API_BASE+API_URL, headers=headers, verify=False)
+        r.raise_for_status()
+    except Exception as e:
+        print(e)
+        exit(1)
+        print("//=======")
+else :
+    pass
+
+
+#5.Check Result
+#GET /api/v1/UserAccounts/Management/Accounts
+headers = {"X-Auth-Token": tokenid, 'Content-Type':'application/json'}
+
+try:
+    API_URL = "/api/v1/UserAccounts/Management/Accounts"
+    r = requests.get(API_BASE+API_URL, headers=headers, verify=False)
+    r.raise_for_status()
+except Exception as e:
+    print(e)
+    exit(1)
+json_response = json.loads(r.text)
+print("-> Check Result")
+acct_list = json_response['Accounts']
+for i in range(len(acct_list)): 
+    print(" - Name : " + str(acct_list[i]['Name']))
+print("//=======")
+
+
---
+++ README.rd
@@ -0,0 +1,16 @@
+Comware7 API for Python
+Account add or del
+
+1. Installation
+python3 -m venv Comware7_API_for_Python
+cd Comware7_API_for_Python
+source bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
+2. Run
+]$ python Comware7_API.py
+python3 Comware7_API.py {add|del} acct_name
+
+
+
